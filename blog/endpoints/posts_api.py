5f22d7e03a4e3a519c8d63120986769d20be46f0
nazo <s3moye@gmail.com>
2025-04-20 00:16:47 +0800

    feat: get_posts_list

--- blog/endpoints/posts_api.py
+++ blog/endpoints/posts_api.py
@@ -1,5 +1,8 @@
-from blacksheep import Response
+from blacksheep import Response, FromQuery
 from blacksheep.server.controllers import APIController, get
+
+from blog.tables import Posts
+from utils.responses import ApiResponse, StatusCode, jsonify
 
 
 class PostsAPI(APIController):
@@ -7,6 +10,24 @@ class PostsAPI(APIController):
     def route(cls) -> str:
         return "posts"
 
-    @get("/test")
-    async def test_endpoint(self) -> Response:
-        return self.json({"message": "Test endpoint is working!"})
+    @get("/list")
+    async def get_list(self, page: FromQuery[int] = FromQuery(1)):
+        _page = page.value
+        if _page < 1:
+            return jsonify(
+                ApiResponse(
+                    code=StatusCode.INVALID_PARAMS,
+                    message="Page must be greater than 0.",
+                )
+            )
+        return jsonify(
+            ApiResponse(
+                code=StatusCode.SUCCESS,
+                data=(
+                    await Posts.select()
+                    .limit(10)
+                    .offset((_page - 1) * 10)
+                    .order_by(Posts.created_at, ascending=False)
+                ),
+            )
+        )
